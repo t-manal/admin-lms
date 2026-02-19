f0a78cb285fdf867b94c8f838a63e9558021d515
mohammed199768 <81799654+mohammed199768@users.noreply.github.com>
2026-02-19 09:24:33 +0300

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+const RIYAL_SIGN = '\u20C1';
+
 export function formatPrice(price: number) {
-    return new Intl.NumberFormat('ar-SA', {
-        style: 'currency',
-        currency: 'SAR',
+    const formattedNumber = new Intl.NumberFormat('ar-SA', {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
     }).format(price);
+
+    return `\u200E${RIYAL_SIGN}\u00A0${formattedNumber}`;
 }
